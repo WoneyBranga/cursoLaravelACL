98c23916160a2ded63b48fe499a904ae0801c73d
woneybranga <woney.branga@gmail.com>
2020-02-14 10:50:23 -0300

    ajuste validacao da edicao

--- resources/views/update-post.blade.php
+++ resources/views/update-post.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-        <h1>{{$post->title}}</h1>
-        <p>{{$post->description}}</p>
-        <b>Autor: {{$post->user->name}}</b>
-        <hr>
-</div>
+    @can('edit_post', $post)
+        <div class="container">
+            <h1>{{$post->title}}</h1>
+            <p>{{$post->description}}</p>
+            <b>Autor: {{$post->user->name}}</b>
+            <hr>
+        </div>
+    @endcan
+    @cannot('edit_post', $post)
+        <p class="alert alert-danger">Edição nao autorizada para este usuario.</p>
+    @endcan
 @endsection
